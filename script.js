df1bc6e006535ac9c52e538dba92e86dc8b46b2c
João Paulo da Silva Sousa <joaopaulodasilvasousa2021@gmail.com>
2025-07-11 21:53:51 -0300

    Adicionando arquivos

---
+++ script.js
@@ -0,0 +1,7 @@
+
+console.log("JavaScript carregado com sucesso!");
+
+// Exemplo: mudar a cor de fundo quando a página carregar
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("Página carregada!");
+});
